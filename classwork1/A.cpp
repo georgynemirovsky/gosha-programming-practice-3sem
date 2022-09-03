c9624c0b761830de0f8cb149c5efb4149b919a72
georgynemirovsky <91407260+georgynemirovsky@users.noreply.github.com>
2022-09-03 14:24:35 +0300

    A

--- classwork1/A.cpp
+++ classwork1/A.cpp
@@ -2,6 +2,17 @@
 
 using namespace std;
 
+void swap(void* x, void* y) {
+    void* t;
+    t = x;
+    x = y;
+    y = t;
+}
+
 int main() {
+    int a, b;
+    cin >> a >> b;
+    swap(a, b);
+    cout << a << " " << b << endl;
     return 0;
 }
