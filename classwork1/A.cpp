538b287390add3dccf99c70c305fcda46ce23c6a
georgynemirovsky <91407260+georgynemirovsky@users.noreply.github.com>
2022-09-04 13:22:39 +0300

    A

--- classwork1/A.cpp
+++ classwork1/A.cpp
@@ -1,18 +1,20 @@
-#include <iostream>
+#include<iostream>
+#include <cstring>
 
 using namespace std;
 
-void swap(void* x, void* y) {
-    void* t;
-    t = x;
-    x = y;
-    y = t;
+void swap (void* a, void* b, int type_size){
+    void* tmp = malloc(type_size);
+    memcpy (tmp, a, type_size );
+    memcpy (a, b, type_size );
+    memcpy (b, tmp, type_size );
+    free(tmp);
 }
 
-int main() {
-    int a, b;
+int main(){
+    char a, b;
     cin >> a >> b;
-    swap(a, b);
+    swap(&a , &b, sizeof(char));
     cout << a << " " << b << endl;
     return 0;
 }
