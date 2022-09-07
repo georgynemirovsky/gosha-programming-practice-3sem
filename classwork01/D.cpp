a1e3ec71abf46a8170deb1ea5af5373633d2f061
georgynemirovsky <91407260+georgynemirovsky@users.noreply.github.com>
2022-09-07 21:30:33 +0300

    D

--- classwork01/D.cpp
+++ classwork01/D.cpp
@@ -4,11 +4,11 @@
 using namespace std;
 
 int main(){
-    cout << "char " << numeric_limits<char>::min() << " " << numeric_limits<char>::max() << " " << sizeof(char) << endl;
-    cout << "short int " << numeric_limits<short int>::min() << " " << numeric_limits<short int>::max() << " " << sizeof(short int) << endl;
-    cout << "int " << numeric_limits<int>::min() << " " << numeric_limits<int>::max() << " " << sizeof(int) << endl;
-    cout << "long int " << numeric_limits<long int>::min() << " " << numeric_limits<long int>::max() << " " << sizeof(long int) << endl;
-    cout << "float " << numeric_limits<float>::min() << " " << numeric_limits<float>::max() << " " << sizeof(float) << endl;
-    cout << "double " << numeric_limits<double>::min() << " " << numeric_limits<double>::max() << " " << sizeof(double) << endl;
+    cout << "char " << numeric_limits<char>::min() << " " << numeric_limits<char>::max() << " " << sizeof(char) * 8 << endl;
+    cout << "short int " << numeric_limits<short int>::min() << " " << numeric_limits<short int>::max() << " " << sizeof(short int) * 8 << endl;
+    cout << "int " << numeric_limits<int>::min() << " " << numeric_limits<int>::max() << " " << sizeof(int) * 8 << endl;
+    cout << "long int " << numeric_limits<long int>::min() << " " << numeric_limits<long int>::max() << " " << sizeof(long int) * 8 << endl;
+    cout << "float " << numeric_limits<float>::min() << " " << numeric_limits<float>::max() << " " << sizeof(float) * 8 << endl;
+    cout << "double " << numeric_limits<double>::min() << " " << numeric_limits<double>::max() << " " << sizeof(double) * 8 << endl;
     return 0;
 }
