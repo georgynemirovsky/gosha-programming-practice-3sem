124ec0041dc9ad2881bbd9e1b1bb77686d3d5cbd
georgynemirovsky <91407260+georgynemirovsky@users.noreply.github.com>
2022-09-07 21:21:33 +0300

    B

--- classwork01/B.cpp
+++ classwork01/B.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+int next_element (void* &current, int type_size){
+    char* cur_ptr = reinterpret_cast<char*>(current);
+    cur_ptr += type_size;
+    current = reinterpret_cast<void*>(cur_ptr);
+    return 0;
+}
+
 int compare_int_min (void* a, void* b){
     if ((*reinterpret_cast<int*>(a)) < (*reinterpret_cast<int*>(b))){
         return 1;
@@ -16,14 +23,7 @@ int compare_int_max (void* a, void* b){
     return 0;
 }
 
-int next_int (void* &current){
-    int* cur_ptr = reinterpret_cast<int*>(current);
-    cur_ptr++;
-    current = reinterpret_cast<void*>(cur_ptr);
-    return 0;
-}
-
-int minimal(void* arr, int arr_size, int type_size, int (*compare)(void *, void *), int (*next)(void * &)) {
+int minimal(void* arr, int arr_size, int type_size, int (*compare)(void *, void *)) {
     int min_index = 0;
     void* min_value = arr;
     for (int i = 0; i < arr_size; i++){
@@ -31,17 +31,25 @@ int minimal(void* arr, int arr_size, int type_size, int (*compare)(void *, void 
             min_index = i;
             min_value = arr;
         }
-        next(arr);
+        next_element(arr, type_size);
     }
     return min_index;
 }
 
 int main(){
-    int arr[10] = {0};
-    for (int i = 0; i < 10; i++){
-        cin >> arr[i];
+    int arr_int[5] = {0};
+    char arr_char[5] = {' '};
+    cout << "Enter 5 integers" << endl;
+    for (int i = 0; i < 5; i++){
+        cin >> arr_int[i];
     }
-    cout << "Index of the minimum element: " << minimal(arr, 10, sizeof(int), compare_int_min, next_int) << endl;
-    cout << "Index of the maximum element: " << minimal(arr, 10, sizeof(int), compare_int_max, next_int) << endl;
+    cout << "Index of the minimum element: " << minimal(arr_int, 5, sizeof(int), compare_int_min) << endl;
+    cout << "Index of the maximum element: " << minimal(arr_int, 5, sizeof(int), compare_int_max) << endl;
+    cout << "Enter 5 chars" << endl;
+    for (int i = 0; i < 5; i++){
+        cin >> arr_char[i];
+    }
+    cout << "Index of the minimum element: " << minimal(arr_int, 5, sizeof(char), compare_int_min) << endl;
+    cout << "Index of the maximum element: " << minimal(arr_int, 5, sizeof(char), compare_int_max) << endl;
     return 0;
 }
